9467b5612ed4bd58877c68ae547ba3a12b6fbf7c
Martin Bordon <martinbordon36@gmail.com>
2020-09-04 02:25:48 -0300

    terminado-controlar-responsive

--- js/script.js
+++ js/script.js
@@ -11,34 +11,41 @@ primera.addEventListener('click', function() {
 })
 
 
-/*
+
 var intentos = 3;
 var inicioIntentos = 0;
 
 function preguntero() {
+
+    var rtaCorrecta = "Gordon Moore";
+    var correcta = "Robert Noyce";
+
+
     if (inicioIntentos >= 3) {
-        alert("PERDISTE...");
-        document.getElementById("#respuesta").disabled;
-
-        var rtaCorrecta = "Gordon Moore"
-        var correcta = "Robert Noyce";
-
-        do {
-            var ingreso = document.getElementById("#respuesta").value;
-
-            if (ingreso === rtaCorrecta || ingreso === correcta) {
-                alert("Felicitaciones, Acertaste. GANASTE TU CUPON DE DESCUENTO")
-            } else {
-                inicioIntentos++;
-            }
-            if (inicioIntentos === 2) {
-                alert("1RA PISTA : PUEDE SER CUALQUIERA DE LOS 2 FUNDADORES. G.... M....")
-            } else if (inicioIntentos === 3) {
-                alert("2DA PISTA: R... N....");
-            }
-            break;
-
-
-        } while (inicioIntentos <= 3);
+        alert("PERDISTE... Las respuestas eran Gordon Moore  o Robert Noyce");
+        document.getElementById("respuesta").disabled;
     }
-    */
+
+    do {
+        var ingreso = document.getElementById("respuesta").value;
+
+        if (ingreso === rtaCorrecta || ingreso === correcta) {
+            alert("Felicitaciones, Acertaste. GANASTE TU CUPON DE DESCUENTO")
+        } else {
+            inicioIntentos++;
+        }
+        if (inicioIntentos === 2) {
+            alert("1RA PISTA : PUEDE SER CUALQUIERA DE LOS 2 FUNDADORES. G.... M....")
+        } else if (inicioIntentos === 3) {
+            alert("2DA PISTA: R... N....");
+        }
+        break;
+
+
+    } while (inicioIntentos <= 3);
+}
+
+if (inicioIntentos >= 3) {
+    alert("PERDISTE...");
+    document.getElementById("respuesta").disabled;
+}
